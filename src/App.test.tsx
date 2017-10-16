5c99163b859e7c08aca75807c76839e75951e9ea
John Johnson <johnrjj@gmail.com>
2017-10-16 19:07:21 -0400

    Fixing smoke test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import { WebSocket, Server, SocketIO } from 'mock-socket';
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-// });
+declare var global: any;
+global.WebSocket= WebSocket
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
